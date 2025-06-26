c2f6ce8aeb0c6b5ee7ce4d30b677b6dc58e70eb5
bellalMohamed <mbellal2000@gmail.com>
2025-06-26 17:12:05 +0100

    setup

--- docker-compose/everything/setup.sh
+++ docker-compose/everything/setup.sh
@@ -67,10 +67,13 @@ install_docker_debian() {
     sudo apt install -y apt-transport-https ca-certificates curl gnupg lsb-release
     
     # Add Docker's official GPG key
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
     
-    # Add Docker repository
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    # Detect distribution codename
+    DISTRO_CODENAME=$(lsb_release -cs)
+    
+    # Add Docker repository for Debian
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $DISTRO_CODENAME stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     
     # Update package index again
     sudo apt update
